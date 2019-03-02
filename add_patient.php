828159e76543a846a06c616026d516093b498db3
OKhomiakova <kolliame@gmail.com>
2019-03-03 02:34:55 +0300

    Minor changes in nav bar

--- add_patient.php
+++ add_patient.php
@@ -10,6 +10,26 @@
 </head>
 <body>
     <?php include 'header.php';?>
+    <nav>
+        <ul>
+            <li class="dropdown"> 
+                <a class="active dropbtn" href="javascript:void(0)">Пациенты</a>
+                <div class="dropdown-content">
+                    <a href="add_patient.php"><i class="fas fa-plus"></i>&nbsp;Новый пациент</a>
+                    <a href="search.php"><i class="fas fa-search"></i>&nbsp;Найти</a>
+                </div>
+            </li>
+            <li><a href="add_result.php">Результаты исследований и назначение терапии</a></li>
+            <li><a href="report.html">Создать отчет</a></li>
+            <li class="dropdown" style="float: right;"> 
+                    <a class="dropbtn" href="javascript:void(0)">Username</a>
+                    <div class="dropdown-content">
+                        <a href=""><i class="fas fa-key"></i>&nbsp;Сменить пароль</a>
+                        <a href=""><i class="fas fa-sign-out-alt"></i>&nbsp;Выйти</a>
+                    </div>
+                </li>
+        </ul>  
+    </nav>
     <form action="./add_data_patient.php" method="POST">
         <fieldset>
             <legend>Паспорт пациента</legend>
---
+++ patient.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Новый пациент</title>
+    <link rel="stylesheet" href=".\style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+<body>
+    <?php include 'header.php';?>
+    <nav>
+        <ul>
+            <li class="dropdown"> 
+                <a class="active dropbtn" href="javascript:void(0)">Пациенты</a>
+                <div class="dropdown-content">
+                    <a href="add_patient.php"><i class="fas fa-plus"></i>&nbsp;Новый пациент</a>
+                    <a href="search.php"><i class="fas fa-search"></i>&nbsp;Найти</a>
+                </div>
+            </li>
+            <li><a href="add_result.php">Результаты исследований и назначение терапии</a></li>
+            <li><a href="report.html">Создать отчет</a></li>
+            <li class="dropdown" style="float: right;"> 
+                    <a class="dropbtn" href="javascript:void(0)">Username</a>
+                    <div class="dropdown-content">
+                        <a href=""><i class="fas fa-key"></i>&nbsp;Сменить пароль</a>
+                        <a href=""><i class="fas fa-sign-out-alt"></i>&nbsp;Выйти</a>
+                    </div>
+                </li>
+        </ul>  
+    </nav>
+</body>
+</html>
