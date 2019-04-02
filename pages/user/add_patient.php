37fed4f57ce53da8277e4004de5036296af5fa31
OKhomiakova <kolliame@gmail.com>
2019-04-02 14:37:05 +0300

    Fixed add patient form design

--- pages/user/add_patient.php
+++ pages/user/add_patient.php
@@ -77,23 +77,33 @@
                         <option value="Вероятная">Вероятная</option>
                     </select>
 
-                    <label for="IBS">ИБС</label>
-                    <div class="align">
-                        <p><input type="radio" id="IBS" name="ibs" value="Есть">Есть</p>
-                        <p><input type="radio" id="IBS" name="ibs" value="Нет" checked>Нет</p>
+                    <div style="display:flex;">
+                        <div style="display:grid; flex: 40%; margin-right: 10px;" >
+                        <fieldset style='width: 93%' >
+                        <legend><label for="IBS">ИБС</label></legend>
+                            <div class="align">
+                                <p><input type="radio" id="IBS" name="ibs" value="Есть">Есть</p>
+                                <p><input type="radio" id="IBS" name="ibs" value="Нет" checked>Нет</p>
+                            </div>
+                        </fieldset>
+                        </div>
+                        <div style="display:grid; flex: 40%; margin-left: 10px;">
+                        <fieldset style='width: 93%' >
+                        <legend><label for="AG">АГ</label></legend>
+                            <div class="align">
+                                <p><input type="radio" id="AG" name="ag" value="Есть">Есть</p>
+                                <p><input type="radio" id="AG" name="ag" value="Нет" checked>Нет</p>
+                            </div>
+                        </fieldset>
+                        </div>
                     </div>
+                  
 
                     <label for="OIM">ОИМ в анамнезе</label>
                     <input type="number" id="OIM" name="oim" min="0" required>
 
                     <label for="operationNum">Операция в анамнезе</label>
                     <input type="number" id="operationNum" name="operationNum" min="0">
-
-                    <label for="AG">АГ</label>
-                    <div class="align">
-                        <p><input type="radio" id="AG" name="ag" value="Есть">Есть</p>
-                        <p><input type="radio" id="AG" name="ag" value="Нет" checked>Нет</p>
-                    </div>
                     
                     <label for="nutStatus">Нутритивный статус</label>
                     <select name="nutStatus" id="nutStatus" required>
@@ -103,28 +113,40 @@
                             <option value="Выраженное ожирение">Выраженное ожирение</option>
                     </select>
                     
-                    <label for="mutation">Мутации</label>
-                    <div class="align">
-                        <p><input type="radio" id="mutation" name="mutation" value="Есть">Есть</p>
-                        <p><input type="radio" id="mutation" name="mutation" value="Нет" selected>Нет</p>
-                    </div>
-                    
                     <label for="height">Рост</label>
                     <input type="number" id="height" name="height" min="100" max="250" required>
                     
-                    <label for="therapyOk">Привержен терапии</label>
-                    <div class="align">
-                        <p><input type="radio" id="therapyOk" name="therapyOk" value="Да" checked>Да</p>
-                        <p><input type="radio" id="therapyOk" name="therapyOk" value="Нет">Нет</p>
+                    <div style="display:flex;">
+                        <div style="display:grid; flex: 40%; margin-right: 10px;" >
+                        <fieldset style='width: 90%' >
+                            <legend><label for="mutation">Мутации</label></legend>
+                            <div class="align">
+                                <p><input type="radio" id="mutation" name="mutation" value="Есть">Есть</p>
+                                <p><input type="radio" id="mutation" name="mutation" value="Нет" checked>Нет</p>
+                            </div>
+                        </fieldset>
+                        </div>
+                        <div style="display:grid; flex: 40%; margin-left: 10px; margin-right: 10px;">
+                        <fieldset style='width: 90%' >
+                            <legend><label for="therapyOk">Привержен терапии</label></legend>
+                            <div class="align">
+                                <p><input type="radio" id="therapyOk" name="therapyOk" value="Да" checked>Да</p>
+                                <p><input type="radio" id="therapyOk" name="therapyOk" value="Нет">Нет</p>
+                            </div>
+                        </fieldset>
+                        </div>
+                        <div style="display:grid; flex: 40%; margin-left: 10px; ">
+                        <fieldset style='width: 90%' >
+                            <legend> <label for="smoking">Курение</label></legend>
+                            <div class="align">
+                                <p><input type="radio" id="smoking" name="smoking" value="Да">Да</p>
+                                <p><input type="radio" id="smoking" name="smoking" value="Нет" checked>Нет</p>
+                            </div>
+                        </fieldset>
+                        </div>
                     </div> 
-                   
-                    <label for="smoking">Курение</label>
-                    <div class="align">
-                        <p><input type="radio" id="smoking" name="smoking" value="Да">Да</p>
-                        <p><input type="radio" id="smoking" name="smoking" value="Нет" checked>Нет</p>
-                    </div>
-                    
-                    <input type="submit" name="submit" value="Внести данные">
+                
+                    <input type="submit" value="Внести данные">
                 </div>
         </fieldset>
         </div>
